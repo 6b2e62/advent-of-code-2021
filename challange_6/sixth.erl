2a68963af2553925d726bdc7be2a42585d14e3e7
kb <6b2e62@gmail.com>
2021-12-07 02:43:25 +0100

    Erlang changed tabs into spaces

--- challange_6/sixth.erl
+++ challange_6/sixth.erl
@@ -10,36 +10,36 @@ load_estimates([], Estimates) -> Estimates;
 load_estimates([InputValue|Rest], Estimates) ->
   Val = maps:get(InputValue, Estimates),
   NewEstimates = maps:update(InputValue, Val + 1, Estimates),
-	load_estimates(Rest, NewEstimates).
+  load_estimates(Rest, NewEstimates).
 
 new_estimate(Key, Estimates, EstimatesCopy) ->
-	Value = maps:get(Key, EstimatesCopy),
-	if
-		Key == 0 ->
-			CurrentValue = maps:get(6, Estimates),
-			UpdatedEstimates = maps:update(6, CurrentValue + Value, Estimates),
-			maps:update(8, Value, UpdatedEstimates);
-		true ->
-			maps:update(Key - 1, Value, Estimates)
-	end.
+  Value = maps:get(Key, EstimatesCopy),
+  if
+    Key == 0 ->
+      CurrentValue = maps:get(6, Estimates),
+      UpdatedEstimates = maps:update(6, CurrentValue + Value, Estimates),
+      maps:update(8, Value, UpdatedEstimates);
+    true ->
+      maps:update(Key - 1, Value, Estimates)
+end.
 
 new_estimate_by_key([], _, Estimates, _) -> Estimates;
 new_estimate_by_key([Key|Rest], Day, Estimates, EstimatesCopy) ->
-	NewEstimates = new_estimate(Key, Estimates, EstimatesCopy),
-	new_estimate_by_key(Rest, Day, NewEstimates, EstimatesCopy).	
+  NewEstimates = new_estimate(Key, Estimates, EstimatesCopy),
+  new_estimate_by_key(Rest, Day, NewEstimates, EstimatesCopy).	
 
 run_estimator([], Estimates, _) -> Estimates;
 run_estimator([Day|Rest], Estimates, Keys) ->
-	NewEstimates = new_estimate_by_key(Keys, Day, Estimates, Estimates),
-	run_estimator(Rest, NewEstimates, Keys).
+  NewEstimates = new_estimate_by_key(Keys, Day, Estimates, Estimates),
+  run_estimator(Rest, NewEstimates, Keys).
 
 main() ->
   Days = 256,
   DaysList = lists:seq(1, Days, 1),	
   Input = read_challange_input(),
   EmptyEstimates = #{ 8 => 0, 7 => 0, 6 => 0, 5 => 0, 4 => 0, 3 => 0, 2 => 0, 1 => 0, 0 => 0},
-	Estimates = load_estimates(Input, EmptyEstimates),
+  Estimates = load_estimates(Input, EmptyEstimates),
   Keys = lists:reverse(maps:keys(EmptyEstimates)),
-	NewEstimates = run_estimator(DaysList, Estimates, Keys),	
-	Sum = maps:values(NewEstimates),
-	io:format("Population: ~p~n", [lists:sum(Sum)]).
+  NewEstimates = run_estimator(DaysList, Estimates, Keys),	
+  Sum = maps:values(NewEstimates),
+  io:format("Population: ~p~n", [lists:sum(Sum)]).
